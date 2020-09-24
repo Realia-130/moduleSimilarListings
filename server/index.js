986e3d7f35bb85beccd8b5abdacddc3b5936fd07
Jacob Berg <coffeesnakes@gmail.com>
2020-09-24 15:27:43 -0700

    basic server set up

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const PORT = 3003;
+
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+const db = require('../database.index.js');
+
+const app = express();
+
+const distDir = path.join(__dirname, '../client/dist');
+
+const publicDir = path.join(__dirname, '../public');
+
+app.listen(PORT, () => (console.log(`listening on PORT: ${PORT}`)));
